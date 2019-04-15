6c9e41a20ee232e14b30833b02a5b24336396acb
Mihainuan De Sá Silva <mihainuan.silva@solutis.com.br>
2019-04-15 17:35:45 -0300

    Adjusted Migrations & ConnStr (Solutis)

--- MyCuscuzeria.Shared/Settings.cs
+++ MyCuscuzeria.Shared/Settings.cs
@@ -2,7 +2,16 @@
 {
     public class Settings
     {
+        /// <summary>
+        /// Home ConnectionString
+        /// </summary>
+        //public static string ConnectionString =
+        //    @"Server=(localdb)\MSSQLLocalDB;Database=MyCuscuzeriaDB;User Id=mihai; Password=mihai; Trusted_Connection=True";
+
+        /// <summary>
+        /// Solutis ConnectionString
+        /// </summary>
         public static string ConnectionString =
-            @"Server=(localdb)\MSSQLLocalDB;Database=MyCuscuzeriaDB;User Id=mihai; Password=mihai";
+            @"Server=(localdb)\v11.0;Database=MyCuscuzeriaDB;User Id=mihai; Password=mihai; Trusted_Connection=True";
     }
 }
