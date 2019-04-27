6d0b309e4728530ed3d16701b99ad1d8fccc6e22
Mihainuan de Sá Silva <mihainuansilva@gmail.com>
2019-04-27 18:08:30 -0300

    Loading commit...

--- myCuscuzeria.App/src/app/home/home.page.ts
+++ myCuscuzeria.App/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(public navCtrl: NavController, private loadingController : LoadingController){
+  }
+
+  searchCuscuz(cuscuz : string){
+    console.log(cuscuz);
+
+    if(cuscuz == null || cuscuz.trim() == ''){
+      return;
+    }
+
+    //Chama a Api e retorna os cuscuzes
+    this.loadCuscuz(cuscuz);
+  }
+
+  //Carrega cuscuzes
+  async loadCuscuz(cuscuz : string){
+    const loading = await this.loadingController.create({
+      spinner: "lines-small",
+      duration: 5000,
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'cuscuz-class cuscuz-loading'
+    });
+    return await loading.present();
+  }
 
 }
